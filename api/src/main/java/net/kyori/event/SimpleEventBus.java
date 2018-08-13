bb1a74018e39d4f74f34c51a8303fc9507d2bf53
Luck <git@lucko.me>
2018-08-13 18:04:35 +0100

    Expose more functionality to SimpleEventBus for querying and modifying current subscribers

--- api/src/main/java/net/kyori/event/SimpleEventBus.java
+++ api/src/main/java/net/kyori/event/SimpleEventBus.java
@@ -24,6 +24,7 @@
 package net.kyori.event;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.SetMultimap;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.function.Predicate;
@@ -44,8 +45,33 @@ public class SimpleEventBus<E> implements EventBus<E> {
     this.registry.unregister(subscriber);
   }
 
+  /**
+   * Unregisters all subscribers matching the {@code predicate}.
+   *
+   * @param predicate the predicate to test subscribers for removal
+   */
   protected void unregisterMatching(final @NonNull Predicate<EventSubscriber<?>> predicate) {
     this.registry.unregisterMatching(predicate);
+  }
+
+  /**
+   * Unregisters all subscribers.
+   */
+  protected void unregisterAll() {
+    this.registry.unregisterAll();
+  }
+
+  /**
+   * Gets an immutable multimap containing all of the subscribers
+   * currently registered.
+   *
+   * <p>Each subscriber is mapped to the type defined when it was
+   * initially {@link #register(Class, EventSubscriber) registered}.</p>
+   *
+   * @return a multimap of the current subscribers
+   */
+  protected @NonNull SetMultimap<Class<?>, EventSubscriber<?>> subscribers() {
+    return this.registry.subscribers();
   }
 
   @Override
