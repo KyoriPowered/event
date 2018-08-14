99aa6b16b8fd0ed0563718ff1c5fb372bc20d6e4
kashike <kashike@vq.lc>
2018-08-14 03:33:10 -0700

    :tada:

---
+++ rx-1/src/main/java/net/kyori/event/rx1/Rx1EventBus.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of event, licensed under the MIT License.
+ *
+ * Copyright (c) 2017-2018 KyoriPowered
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.kyori.event.rx1;
+
+import net.kyori.event.EventSubscriber;
+import net.kyori.event.SimpleEventBus;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import rx.Emitter;
+import rx.Observable;
+import rx.functions.Action1;
+
+/**
+ * An event bus that can be used with RxJava 1.
+ *
+ * @param <E> the event type
+ */
+public class Rx1EventBus<E> extends SimpleEventBus<E> {
+  /**
+   * Creates an observable for {@code event}.
+   *
+   * @param event the event
+   * @param <T> the event type
+   * @return an observable
+   */
+  public <T extends E> @NonNull Observable<T> observable(final @NonNull Class<T> event) {
+    return this.observable(emitter -> {
+      final EventSubscriber<T> subscriber = e -> {
+        try {
+          emitter.onNext(e);
+        } catch(final Throwable t) {
+          emitter.onError(t);
+        }
+      };
+      this.register(event, subscriber);
+      emitter.setCancellation(() -> this.unregister(subscriber));
+    });
+  }
+
+  /**
+   * Creates an observable for {@code event}.
+   *
+   * @param emitter the emitter
+   * @param <T> the event type
+   * @return an observable
+   */
+  protected <T extends E> @NonNull Observable<T> observable(final @NonNull Action1<Emitter<T>> emitter) {
+    return Observable.create(emitter, Emitter.BackpressureMode.BUFFER);
+  }
+}
